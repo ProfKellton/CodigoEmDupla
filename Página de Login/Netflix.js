5a44c83ab359796f8dbc8eb07a163bf82048dff1
Professor Rodrigo <professor.kelltonr@gmail.com>
2025-03-21 10:49:40 -0300

    Adição da página de login da Netflix

---
+++ Página de Login/Netflix.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aqui você pode adicionar sua lógica de autenticação
+    alert('Formulário enviado!');
+});
